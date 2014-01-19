55171d86c4ab00a7615c133a267ae65f42f94956
Timothy Crosley <timothy.crosley@gmail.com>
2014-01-19 16:20:21 -0500

    Implement catogirized error codes in a backward-compatibility way

--- frosted/checker.py
+++ frosted/checker.py
@@ -244,7 +244,7 @@ class Checker(object):
 
     def __init__(self, tree, filename='(none)', builtins=None, **settings):
         self.settings = settings
-        self.ignore_errors = [int(error_code) for error_code in settings.get('ignore_frosted_errors', [])]
+        self.ignore_errors = settings.get('ignore_frosted_errors', [])
         self._node_handlers = {}
         self._deferred_functions = []
         self._deferred_assignments = []
@@ -326,8 +326,12 @@ class Checker(object):
         self.push_scope(ClassScope)
 
     def report(self, message_class, *args, **kwargs):
-        if not message_class.error_code in self.ignore_errors:
+        error_code = message_class.error_code
+
+        if(not error_code[:2] + "00" in self.ignore_errors and not error_code in self.ignore_errors and not
+           str(message_class.error_number) in self.ignore_errors):
             self.messages.append(message_class(self.filename, *args, **kwargs))
+
 
     def has_parent(self, node, kind):
         while hasattr(node, 'parent'):
