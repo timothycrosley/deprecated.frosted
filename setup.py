f2c9382e62c87dce155d9426a37a23ea253eccff
Timothy Crosley <timothy.crosley@gmail.com>
2014-01-28 23:05:01 -0500

    Bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ except (IOError, ImportError, OSError, RuntimeError):
 
 setup(name="frosted",
       license="MIT",
-      version="1.2.1",
+      version="1.3.0",
       description="A passive Python syntax checker",
       long_description=readme,
       author="Timothy Crosley",
